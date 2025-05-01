13fa700c966ba68413750ee4d51ac75e9cb30669
Miranda Johnson <mj592@drexel.edu>
2025-04-30 20:22:32 -0400

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Welcome to Habit Tap!</Text>
+      <Text>Welcome to HabitTap!</Text>
       <Text>This is Dallas's Change</Text>
       <StatusBar style="auto" />
     </View>
