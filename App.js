6cfad8c87bce8ed2bb2a8684332c507a10bdc285
Disha Patel <52142361+avocoder123@users.noreply.github.com>
2025-06-04 00:21:45 -0400

    Update App.js

--- App.js
+++ App.js
@@ -2,12 +2,22 @@ import React from 'react';
 import { SafeAreaView, StyleSheet } from 'react-native';
 import { globalStyles } from './styles';
 import DailyGoalsScreenScreen from './screens/DailyGoalsScreen';
+import LoginScreen from './screens/login';
+import SignupScreen from './screens/signup';
+import HomeScreen from './screens/homescreen';
 
 export default function App() {
   return (
     <SafeAreaView style={globalStyles.container}>
       <DailyGoalsScreenScreen />
     </SafeAreaView>
+   <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Signup" component={SignupScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
